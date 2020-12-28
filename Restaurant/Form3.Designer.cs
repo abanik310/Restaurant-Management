b24ae3241b20c597725a1ced85417ac6bf5c0053
17-33684-1@student.aiub.edu <17-33684-1@student.aiub.edu>
2020-12-29 01:00:59 +0600

    Day 1.2

--- Restaurant/Form3.Designer.cs
+++ Restaurant/Form3.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(893, 497);
+            this.ClientSize = new System.Drawing.Size(1010, 557);
             this.Name = "Form3";
             this.Text = "Form3";
             this.ResumeLayout(false);
