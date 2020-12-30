0c63d7a90f0d3cbf6e7894ad0d8b1d513960443f
Abu Bakar Siddiq <18-38030-2@student.aiub.edu>
2020-12-30 18:32:21 +0600

    UC

--- Restaurant/Presentation/UCmainDish.Designer.cs
+++ Restaurant/Presentation/UCmainDish.Designer.cs
@@ -47,20 +47,20 @@
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(3, 2);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Location = new System.Drawing.Point(2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(602, 494);
+            this.panel1.Size = new System.Drawing.Size(452, 419);
             this.panel1.TabIndex = 0;
             // 
             // button4
             // 
             this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
             this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button4.Location = new System.Drawing.Point(424, 281);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Location = new System.Drawing.Point(318, 228);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(165, 193);
+            this.button4.Size = new System.Drawing.Size(124, 157);
             this.button4.TabIndex = 17;
             this.button4.UseVisualStyleBackColor = true;
             // 
@@ -68,10 +68,10 @@
             // 
             this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
             this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button5.Location = new System.Drawing.Point(216, 281);
-            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Location = new System.Drawing.Point(162, 228);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(165, 193);
+            this.button5.Size = new System.Drawing.Size(124, 157);
             this.button5.TabIndex = 16;
             this.button5.UseVisualStyleBackColor = true;
             // 
@@ -79,10 +79,10 @@
             // 
             this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
             this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button6.Location = new System.Drawing.Point(13, 281);
-            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button6.Location = new System.Drawing.Point(10, 228);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(164, 193);
+            this.button6.Size = new System.Drawing.Size(123, 157);
             this.button6.TabIndex = 15;
             this.button6.UseVisualStyleBackColor = true;
             // 
@@ -90,10 +90,10 @@
             // 
             this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
             this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button3.Location = new System.Drawing.Point(424, 20);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Location = new System.Drawing.Point(318, 16);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(165, 193);
+            this.button3.Size = new System.Drawing.Size(124, 157);
             this.button3.TabIndex = 14;
             this.button3.UseVisualStyleBackColor = true;
             // 
@@ -101,10 +101,10 @@
             // 
             this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button2.Location = new System.Drawing.Point(216, 20);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(162, 16);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(165, 193);
+            this.button2.Size = new System.Drawing.Size(124, 157);
             this.button2.TabIndex = 13;
             this.button2.UseVisualStyleBackColor = true;
             // 
@@ -112,21 +112,21 @@
             // 
             this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.Location = new System.Drawing.Point(13, 20);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(10, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(164, 193);
+            this.button1.Size = new System.Drawing.Size(123, 157);
             this.button1.TabIndex = 12;
             this.button1.UseVisualStyleBackColor = true;
             // 
             // MainDishUSCntrl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainDishUSCntrl";
-            this.Size = new System.Drawing.Size(605, 496);
+            this.Size = new System.Drawing.Size(454, 403);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
